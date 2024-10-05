5b1e35b72760000ccd0bbc5e3d7eb5459896f3cd
Valerie Finnemeyer <valerie.finnemeyer@gmail.com>
2024-10-05 15:25:46 -0400

    Completed Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2: Lexical Scoping
 
-## Write a short comment describing this function
-
+## Creates special matrix object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmat <- function(solve) m <<- solve
+  getmat <- function() m
+  list(set = set, get = get,
+       setmat = setmat,
+       getmat = getmat)
 }
 
 
-## Write a short comment describing this function
-
+## Computes inverse of matrix returned by above
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getmat()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmat(m)
+  m
 }
